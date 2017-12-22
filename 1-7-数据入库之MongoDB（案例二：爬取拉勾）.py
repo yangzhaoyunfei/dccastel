38693c76944d87481a42b658eb624f3d09ae76b8
yangzhaoyunfei <yangzhaoyunfei@qq.com>
2017-12-22 12:25:47 +0800

    '修改拉勾'

--- 1-7-数据入库之MongoDB（案例二：爬取拉勾）.py
+++ 1-7-数据入库之MongoDB（案例二：爬取拉勾）.py
@@ -5,30 +5,31 @@ from fake_useragent import UserAgent
 
 client = MongoClient()
 db = client.lagou
-lagou = db.java  # 创建名为java集合
+lagou = db.bigdata  # 创建名为java集合
 
 headers = {
-    'Cookie': '',
-    'Referer': '',
-}  # 对应的headers信息
+    'Cookie': 'user_trace_token=20171210195058-6356f9ff-dda0-11e7-8dc4-525400f775ce; LGUID=20171210195058-6356fe14-dda0-11e7-8dc4-525400f775ce; _ga=GA1.2.2037944954.1512906660; _gid=GA1.2.1585272669.1513682022; ab_test_random_num=0; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; index_location_city=%E4%B8%8A%E6%B5%B7; hasDeliver=36; LGSID=20171222112023-0c1efd07-e6c7-11e7-a4eb-525400f775ce; PRE_UTM=; PRE_HOST=; PRE_SITE=; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; JSESSIONID=ABAAABAAADEAAFIA9E31557E7F70602314FF28DC7D6139A; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1513830884,1513831709,1513912821,1513913767; _putrc=BB71D504DD84954D; login=true; unick=%E5%94%90%E5%BF%A0%E7%BB%B4; TG-TRACK-CODE=search_code; _gat=1; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1513913857; LGRID=20171222113739-759436ab-e6c9-11e7-a4f0-525400f775ce; SEARCH_ID=802b2fecfe27437582eb70c78d4d10f0',
+    'Referer': 'https://www.lagou.com/jobs/list_%E7%88%AC%E8%99%AB?labelWords=&fromSearch=true&suginput=',
+    # 拉勾此时要验证这个参数
+}  # 填入你抓包的headers信息
 
 
 def get_job_info(page, kd):  # 获取数据
     for i in range(page):
-        url = 'https://www.lagou.com/jobs/positionAjax.json?needAddtionalResult=false&isSchoolJob=0'
-        payload = {
-            'first': 'true',
+        url = 'https://www.lagou.com/jobs/positionAjax.json?city=上海&needAddtionalResult=false&isSchoolJob=0'  # 直接请求的json
+        payload = {  # 要发送的翻页数据
+            'first': 'false',
             'pn': i,
             'kd': kd,
         }
 
         ua = UserAgent()
         headers['User-Agent'] = ua.random  # 使用fake-Agent随机生成User-Agent，添加到headers
-        response = requests.post(url, data=payload, headers=headers)
+        response = requests.post(url, data=payload, headers=headers)  # 直接请求的json数据
 
         if response.status_code == 200:
-            job_json = response.json()['content']['positionResult']['result']  # 解析数据
-            lagou.insert(job_json)  # 保存数据
+            job_json = response.json()['content']['positionResult']['result']  # 解析json字典中想要的键
+            lagou.insert(job_json)  # 保存数据，入mongodb
         else:
             print('Something Wrong!')
 
@@ -37,4 +38,4 @@ def get_job_info(page, kd):  # 获取数据
 
 
 if __name__ == '__main__':
-    get_job_info(3, 'java')  # 爬取前3页的PHP职位信息
+    get_job_info(31, '大数据')  # 爬取前3页的PHP职位信息
--- XX.py
+++ XX.py
@@ -1,50 +1,22 @@
-# 定义一个“Animal”类，具有吃、喝、拉、撒的功能
-class Animal:
-    name = ''
+from pymongo import MongoClient
+import requests
 
-    def eat(self):
-        print("%s 吃 " % self.name)
+client = MongoClient()
+db = client.lagou  # 创建一个lagou数据库
+my_set = db.job  # 创建job集合
 
-    def drink(self):
-        print("%s 喝 " % self.name)
+url = 'https://www.lagou.com/jobs/positionAjax.json?city=%E4%B8%8A%E6%B5%B7&needAddtionalResult=false&isSchoolJob=0'
+payload = {
+    'first': 'true',
+    'pn': '1',
+    'kd': '{}'format(kd),
+}
 
-    def shit(self):
-        print("%s 拉 " % self.name)
+headers = {
+    'Cookie': 'user_trace_token=20171210195058-6356f9ff-dda0-11e7-8dc4-525400f775ce; LGUID=20171210195058-6356fe14-dda0-11e7-8dc4-525400f775ce; _ga=GA1.2.2037944954.1512906660; _gid=GA1.2.1585272669.1513682022; ab_test_random_num=0; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; index_location_city=%E4%B8%8A%E6%B5%B7; hasDeliver=36; LGSID=20171222112023-0c1efd07-e6c7-11e7-a4eb-525400f775ce; PRE_UTM=; PRE_HOST=; PRE_SITE=; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; JSESSIONID=ABAAABAAADEAAFIA9E31557E7F70602314FF28DC7D6139A; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1513830884,1513831709,1513912821,1513913767; _putrc=BB71D504DD84954D; login=true; unick=%E5%94%90%E5%BF%A0%E7%BB%B4; TG-TRACK-CODE=search_code; _gat=1; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1513913857; LGRID=20171222113739-759436ab-e6c9-11e7-a4f0-525400f775ce; SEARCH_ID=802b2fecfe27437582eb70c78d4d10f0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+    'Referer': 'https://www.lagou.com/jobs/list_%E7%88%AC%E8%99%AB?labelWords=&fromSearch=true&suginput=',
+}  # 填入对应的headers信息
 
-    def pee(self):
-        print("%s 撒 " % self.name)
-
-    # 定义一个“Cat”类，继承“Animal”类所有功能，同时添加属于猫的“喵喵叫”功能
-
-
-class Cat(Animal):
-
-    def __init__(self, name):
-        self.name = name
-        self.breed = '猫'
-
-    def cry(self):
-        print('喵喵叫')
-
-
-# 定义一个“Dog”类，继承“Animal”类所有功能，同时添加属于狗的“汪汪叫”功能
-class Dog(Animal):
-
-    def __init__(self, name):
-        self.name = name
-        self.breed = '狗'
-
-    def cry(self):
-        print('汪汪叫')
-
-
-# ######### 执行 #########
-
-c1 = Cat('小白家的小黑猫')
-c1.eat()
-
-c2 = Cat('小黑的小白猫')
-c2.drink()
-
-d1 = Dog('胖子家的小瘦狗')
-d1.eat()
+response = requests.post(url, data=payload, headers=headers)  # 使用POST方法请求数据，加上payload和headers信息
+print(response.json())  # 把对应的数据保存到MOngoDB
